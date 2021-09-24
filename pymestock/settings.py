bcfc9a3b5ab927037a017d06ff6176cda4d4b9bb
Invokeichon <invokeichon@gmail.com>
2021-09-24 09:14:11 -0300

    fixed installedApps urls on settings.py

--- pymestock/settings.py
+++ pymestock/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "pymestock.apps.account.apps.AccountConfig",
-    "pymestock.apps.business.apps.BusinessConfig",
-    "pymestock.apps.inventory.apps.InventoryConfig",
+    "pymestock.apps.account",
+    "pymestock.apps.business",
+    "pymestock.apps.inventory",
 ]
 
 MIDDLEWARE = [
