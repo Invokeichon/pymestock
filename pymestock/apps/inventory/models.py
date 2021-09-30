6c91c2fbb50a6ae17908c8c02d5d961f7ed3738c
unknown <saaa.arellano@gmail.com>
2021-09-30 19:14:01 -0300

    The data basis were migrated.

--- pymestock/apps/inventory/models.py
+++ pymestock/apps/inventory/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from pymestock.apps.business.models import Company
 
-# Create your models here.
+
+CharField_max_length = 100
+
+
+class Item(models.Model):
+    id_item = models.IntegerField()
+    id_company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=CharField_max_length)
+    brand = models.CharField(max_length=CharField_max_length)
+    price = models.IntegerField()
+    description = models.CharField(max_length=CharField_max_length)
+    category = models.CharField(max_length=CharField_max_length)
+    photo = models.ImageField()
+
+
+class Inventory(models.Model):
+    id_item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    stock_actual = models.IntegerField()
+
+
+class MeasureUnit(models.Model):
+    category = models.CharField(max_length=CharField_max_length)
+    name = models.CharField(max_length=CharField_max_length)
+    abbreviation = models.CharField(max_length=CharField_max_length)
