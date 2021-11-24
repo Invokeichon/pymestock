dbd3ea06a6dc4dc8de2b6083beaba04c087a7322
Matías <70768219+Invokeichon@users.noreply.github.com>
2021-11-24 18:54:31 -0300

    added templates for adding item to inventory, and user dashboard (#7)

--- pymestock/apps/inventory/urls.py
+++ pymestock/apps/inventory/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("inventory.html", views.inventory, name="inventory"),
+    path("inventory/add_item.html", views.add_item, name="add_item")
 ]
--- pymestock/apps/inventory/views.py
+++ pymestock/apps/inventory/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def inventory(request):
     return render(request, "inventory/inventory.html", {})
+
+def add_item(request):
+    return render(request, "inventory/add_item.html", {})
