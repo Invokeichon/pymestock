190f25ffc443658a44e56fe99101c44893239b59
estebanzuniga <esteban.zuniga@ug.uchile.cl>
2021-11-24 22:26:46 -0300

    backend v1

--- pymestock/apps/inventory/models.py
+++ pymestock/apps/inventory/models.py
@@ -6,14 +6,12 @@ CharField_max_length = 100
 
 
 class Item(models.Model):
-    id_item = models.IntegerField()
     id_business = models.ForeignKey(Business, on_delete=models.CASCADE)
     name = models.CharField(max_length=CharField_max_length)
     brand = models.CharField(max_length=CharField_max_length)
     price = models.IntegerField()
     description = models.CharField(max_length=CharField_max_length)
     category = models.CharField(max_length=CharField_max_length)
-    photo = models.ImageField()
 
 
 class Inventory(models.Model):
--- pymestock/apps/inventory/views.py
+++ pymestock/apps/inventory/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
-
+from .models import Item, Inventory, MeasureUnit
+from pymestock.apps.business.models import Business
 
 def inventory(request):
-    return render(request, "inventory/inventory.html", {})
+    if request.user.is_authenticated:
+        mis_items = Item.objects.filter(id_business = Business.objects.filter(owner = request.user))
+        return render(request, "inventory/inventory.html", {"inventory": mis_items})
+    else:
+        return render(request, "../../templates/account/login.html")
+
+def add_item(request):
+
+    if request.method == "GET":
+        return render(request, "inventory/add_item.html")
+
+    if request.method == "POST":
+        if "Agregar item" in request.POST:
+            id_business = Business.objects.filter(owner = request.user)
+            name = request.POST["name"]
+            brand = request.POST["brand"]
+            price = request.POST["price"]
+            description = request.POST["description"]
+            category = request.POST["category"]
+
+            if request.user.is_authenticated:
+                nuevo_item = Item(id_business=id_business, name=name, brand=brand, price=price,
+                                  description=description, category=category)
+                nuevo_item.save()
+                mis_items = Item.objects.filter(id_business = Business.objects.filter(owner = request.user))
+                return render(request, "inventory/inventory.html", {"inventory": mis_items})
+            else:
+                return render(request, "../../templates/account/login.html")
