31169ad775117a685aef6343a780794a82a9e2f6
estebanzuniga <esteban.zuniga@ug.uchile.cl>
2021-11-26 13:19:50 -0300

    added inventory and edit inventory

--- pymestock/apps/inventory/models.py
+++ pymestock/apps/inventory/models.py
@@ -7,14 +7,8 @@ CharField_max_length = 100
 
 class Item(models.Model):
     business = models.ForeignKey(Business, null=True, on_delete=models.CASCADE)
-    sku = models.CharField(max_length=12, null=True)
     name = models.CharField(max_length=CharField_max_length)
     brand = models.CharField(max_length=CharField_max_length)
     price = models.IntegerField()
     description = models.CharField(max_length=CharField_max_length)
-    category = models.CharField(max_length=CharField_max_length)
-
-
-class Inventory(models.Model):
-    id_item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    stock_actual = models.IntegerField()
+    stock_actual = models.IntegerField(null=True)
